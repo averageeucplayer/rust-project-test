7f4a8dc9af683e8e6350394a73e9fdeb2647fc6d
Charlatan <legitimatecharlatan@gmail.com>
2024-12-07 18:01:40 +0000

    clean

--- web-component/src/index.ts
+++
@@ -1,3 +0,0 @@
-import 'svelte';
-export { default as Button } from './Button.svelte';
-export { default as View } from './View.svelte';
--- web-lib/src/types.ts
+++
@@ -1,4 +0,0 @@
-export interface AppRecord {
-    id: number,
-    name: string
-}
